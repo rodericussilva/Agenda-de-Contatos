93db7747d50e522fc5533ed9faf6eec74d992bfd
Rodrigo Silva <112216918+rodericussilva@users.noreply.github.com>
2023-11-20 11:43:55 -0300

    initial commit

---
+++ Project1.dpr
@@ -0,0 +1,16 @@
+program Project1;
+
+uses
+  Vcl.Forms,
+  AgendaContatos in 'AgendaContatos.pas' {Form1},
+  unitDM in 'unitDM.pas' {DM: TDataModule};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.CreateForm(TDM, DM);
+  Application.Run;
+end.
